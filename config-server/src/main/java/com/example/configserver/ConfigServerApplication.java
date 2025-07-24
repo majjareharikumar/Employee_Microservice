80f4066d3281b4454ae964bb4e9cc56644d31e5e
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-07-24 21:57:08 +0530

    Config

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -1,15 +1,17 @@
-package com.example.department_service;
+package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class DepartmentServiceApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
