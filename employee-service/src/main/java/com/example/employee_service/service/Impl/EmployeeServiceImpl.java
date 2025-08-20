7ed9a0a4d206ceff6b8c93b9e234491e022bcaa2
harirebel455 <79778945+harirebel455@users.noreply.github.com>
2025-08-20 23:02:37 +0530

    CircuteBreacker

--- employee-service/src/main/java/com/example/employee_service/service/Impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/example/employee_service/service/Impl/EmployeeServiceImpl.java
@@ -7,7 +7,11 @@ import com.example.employee_service.exception.EmployeeException;
 import com.example.employee_service.repository.EmployeeRepo;
 import com.example.employee_service.service.APIClient;
 import com.example.employee_service.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,10 +23,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
-
+private static final Logger LOGGER= LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepo employeeRepo;
 
-    //private WebClient webClient;
+    private WebClient webClient;
     private APIClient apiClient;
 
     @Override
@@ -47,8 +51,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return savedEmployeeDto;
     }
 
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getdefaultdepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getdefaultdepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("Inside getEmployeeById method");
+
 
         Employee employee=employeeRepo.findById(employeeId).orElseThrow(
                 ()-> new EmployeeException("fistName","lastName",employeeId)
@@ -58,12 +66,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 //
 //        DepartmentDto departmentDto=responseEntity.getBody();
 
-//        ResponseEntity<DepartmentDto> responseEntity = webClient.get()
-//                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
-//                .retrieve()
-//                .toEntity(DepartmentDto.class)
-//                .block();
-        ResponseEntity<DepartmentDto> responseEntity = apiClient.getDepartment(employee.getDepartmentCode());
+        ResponseEntity<DepartmentDto> responseEntity = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .toEntity(DepartmentDto.class)
+                .block();
+//        ResponseEntity<DepartmentDto> responseEntity = apiClient.getDepartment(employee.getDepartmentCode());
 
         DepartmentDto departmentDto = responseEntity.getBody();
 
@@ -80,4 +88,33 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return apiResponseDto;
     }
+    public APIResponseDto getdefaultdepartment(Long employeeId , Exception exception){
+        LOGGER.info("Inside getdefaultdepartment method");
+        Employee employee=employeeRepo.findById(employeeId).orElseThrow(
+                ()-> new EmployeeException("fistName","lastName",employeeId)
+        );
+
+
+        DepartmentDto departmentDto=new DepartmentDto();
+        departmentDto.setId(0L);
+        departmentDto.setDepartmentName("R&D DEPT");
+        departmentDto.setDepartmentDescription("R&D DEPT");
+        departmentDto.setDepartmentCode("R&D");
+
+        EmployeeDto employeeDto=new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+        APIResponseDto apiResponseDto=new APIResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+
+        return apiResponseDto;
+    }
+
+
+
 }
